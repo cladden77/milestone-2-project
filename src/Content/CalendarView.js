07781157d740308e1c309ef18cb0c1e584a311d6
John Weilert <mattweilert@gmail.com>
2022-08-14 23:53:23 -0500

    Added a calendar Css page with notes

--- src/Content/CalendarView.js
+++ src/Content/CalendarView.js
@@ -1,25 +1,29 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "../CSS/calendar.css";
 
 function CalendarView() {
   const [date, setDate] = useState(new Date());
 
   function onClickDay(date) {
-    alert(date)
+    alert(date);
   }
 
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-8">
-          <div className='app'>
-            <h1 className='text-center'>Schedule an Appointment Below:</h1>
-            <div className='calendar-container'>
-              <Calendar onChange={setDate} value={date} onClickDay={onClickDay}/>
+          <div className="app">
+            <h1 className="text-center">Schedule an Appointment Below:</h1>
+            <div className="calendar-container">
+              <Calendar
+                onChange={setDate}
+                value={date}
+                onClickDay={onClickDay}
+              />
             </div>
-            <p className='text-center'>
-              <span className='bold'>Selected Date:</span>{' '}
-              {date.toDateString()}
+            <p className="text-center">
+              <span className="bold">Selected Date:</span> {date.toDateString()}
             </p>
           </div>
         </div>
@@ -28,4 +32,4 @@ function CalendarView() {
   );
 }
 
-export default CalendarView; 
+export default CalendarView;
