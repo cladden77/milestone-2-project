f6b24ff56ad8f70153c7c533e7ee84d5163b261d
deankp <dperez0689@gmail.com>
2022-08-21 14:06:23 -0500

    updated hero sizing

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,11 +4,12 @@ import { MDBBtn } from "mdb-react-ui-kit";
 function Hero() {
   return (
     <div
+      id="mainCarImg"
       className="p-5 text-center bg-image"
       style={{
         backgroundImage:
           "url('https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1583&q=80')",
-        height: "400px",
+        height: "20em",
       }}
     >
       <div className="d-flex justify-content-center align-items-center h-100">
