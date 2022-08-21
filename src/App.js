a866579715124c221be1c783af71ded0893dc6f0
deankp <dperez0689@gmail.com>
2022-08-21 11:51:15 -0500

    added links to Gallery and About Us

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AppointmentList from "./components/appointmentList";
 import Edit from "./components/edit";
+import Gallery from "./components/Gallery";
+import About from "./components/About";
 import Create from "./components/create";
 import Footer from "./components/Footer";
 
@@ -21,6 +23,8 @@ const App = () => {
           <Route exact path="/" element={<AppointmentList />} />
           <Route path="/edit/:id" element={<Edit />} />
           <Route path="/appointment/add" element={<Create />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/About" element={<About />} />
         </Routes>
       </div>
       <Footer />
