3a3e39501e0e4aff06b9d4f0feb254ac443ce28d
deankp <dperez0689@gmail.com>
2022-08-21 10:46:10 -0500

    added a footer and updated Book Appointment Button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from "react";
- 
+
 // We use Route in order to define the different routes of our application
 import { Route, Routes } from "react-router-dom";
- 
+
 // We import all the components we need in our app
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AppointmentList from "./components/appointmentList";
 import Edit from "./components/edit";
 import Create from "./components/create";
- 
+import Footer from "./components/Footer";
+
 const App = () => {
- return (
-  <div className="App">
-   <div>
-     <Navbar />
-     <Hero />
-     <Routes>
-       <Route exact path="/" element={<AppointmentList />} />
-       <Route path="/edit/:id" element={<Edit />} />
-       <Route path="/appointment/add" element={<Create />} />
-     </Routes>
-   </div>
-   </div>
- );
+  return (
+    <div className="App">
+      <div>
+        <Navbar />
+        <Hero />
+        <Routes>
+          <Route exact path="/" element={<AppointmentList />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/appointment/add" element={<Create />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 };
- 
+
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { MDBFooter } from "mdb-react-ui-kit";
+
+function Footer() {
+  return (
+    <MDBFooter color="bg-dark" dark className="text-center text-lg-left">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a className="text-white" href="http://localhost:3000">
+          JDPC Established 2022
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default Footer;
