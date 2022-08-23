ab3936025309733a8cdb587a3af38a450c74ab22
deankp <dperez0689@gmail.com>
2022-08-22 22:11:14 -0500

    slight updates

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import './App.css';
-import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
-import {useState,useEffect} from 'react';
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Register from "./Register";
 import UserContext from "./UserContext";
 import axios from "axios";
 import Login from "./Login";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import Edit from "./components/edit";
+import Gallery from "./components/Gallery";
+import About from "./components/About";
+import Create from "./components/create";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 function App() {
-  const [email,setEmail] = useState('');
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:3000/user', {withCredentials:true})
-      .then(response => {
+    axios
+      .get("http://localhost:3000/user", { withCredentials: true })
+      .then((response) => {
         setEmail(response.data.email);
       });
   }, []);
 
   function logout() {
-    axios.post('http://localhost:3000/logout', {}, {withCredentials:true})
-      .then(() => setEmail(''));
+    axios
+      .post("http://localhost:3000/logout", {}, { withCredentials: true })
+      .then(() => setEmail(""));
   }
 
   return (
     <div className="App">
+      <div>
       <Navbar />
+      <Hero />
       <div className="Container my-5">
-        <UserContext.Provider value={{email,setEmail}}>
+        <UserContext.Provider value={{ email, setEmail }}>
           <BrowserRouter>
             <div>
               {!!email && (
@@ -35,23 +45,27 @@ function App() {
                   <button onClick={() => logout()}>Log out</button>
                 </div>
               )}
-              {!email && (
-                <div>Not logged in</div>
-              )}
+              {!email && <div>Not logged in</div>}
             </div>
-            <hr/>
+            <hr />
             <div>
-              <Link to={'/login'}>Login</Link> | 
-              <Link to={'/register'}> Register</Link>
+              <Link to={"/login"}>Login</Link> |
+              <Link to={"/register"}> Register</Link>
             </div>
             <Routes>
-              <Route path={'/register'} element={<Register/>} />
-              <Route path={'/login'} element={<Login/>} />
+              <Route path={"/register"} element={<Register />} />
+              <Route path={"/login"} element={<Login />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/appointment/add" element={<Create />} />
+              <Route path="/Gallery" element={<Gallery />} />
+              <Route path="/About" element={<About />} />
             </Routes>
-            <hr/>
+            <hr />
           </BrowserRouter>
         </UserContext.Provider>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -1,50 +1,58 @@
-import {useState, useContext} from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import UserContext from "./UserContext";
 
 function Login() {
-
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [loginError,setLoginError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginError, setLoginError] = useState(false);
 
   const user = useContext(UserContext);
-  
 
   function loginUser(e) {
     e.preventDefault();
 
-    const data = {email,password};
-    axios.post('http://localhost:3000/login', data, {withCredentials:true})
-      .then(response => {
+    const data = { email, password };
+    axios
+      .post("http://localhost:3000/login", data, { withCredentials: true })
+      .then((response) => {
         user.setEmail(response.data.email);
-        setEmail('');
-        setPassword('');
+        setEmail("");
+        setPassword("");
         setLoginError(false);
       })
       .catch(() => {
         setLoginError(true);
       });
   }
-  
+
   const navigate = useNavigate();
 
   return (
-    
     <div className="my-5">
-    <form action="" onSubmit={e => loginUser(e)}>
-      {loginError && (
-        <div>LOGIN ERROR! WRONG EMAIL OR PASSWORD!</div>
-      )}
-      <div className="my-3">
-      <input type="email" placeholder="email" value={email} onChange={e => setEmail(e.target.value)}/>
-      </div>
-      <div className="mb-3">
-      <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)}/>
-      </div>
-      <button type="submit" onClick={() => navigate('/appointment/add')}>log in</button>
-    </form>
+      <form action="" onSubmit={(e) => loginUser(e)}>
+        {loginError && <div>LOGIN ERROR! WRONG EMAIL OR PASSWORD!</div>}
+        <div className="my-3">
+          <input
+            type="email"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" onClick={() => navigate("/")}>
+          log in
+        </button>
+      </form>
     </div>
   );
 }
